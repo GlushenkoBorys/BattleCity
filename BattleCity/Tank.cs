41818ce075d80d2ef4f31fb57292da84fecfc95d
Borys <B.Glushchenko21@gmail.com>
2021-06-01 13:37:29 +0300

    Made class Tank

---
+++ BattleCity/Tank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCity
+{
+     abstract class Tank
+    {
+        int body;
+        int tower;
+        int gun;
+        int chassis;
+    }
+}
